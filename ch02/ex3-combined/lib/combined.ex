647d419c67ab8843428b69b448f044ccb331a006
Jiawei Cui <jiaweicui@juniper.net>
2017-04-12 11:38:23 -0700

    ex3-combined kicked off

---
+++ ch02/ex3-combined/lib/combined.ex
@@ -0,0 +1,18 @@
+defmodule Combined do
+  @moduledoc """
+  Documentation for Combined.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Combined.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
