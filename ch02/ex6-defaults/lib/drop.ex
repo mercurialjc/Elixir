6da6930f212ab39364d898709c30462b82d47269
Jiawei Cui <jiaweicui@juniper.net>
2017-04-12 17:13:07 -0700

    ex6-default completed

---
+++ ch02/ex6-defaults/lib/drop.ex
@@ -0,0 +1,30 @@
+defmodule Drop do
+  @moduledoc """
+  Module Drop for calculating fall velocities, with a default value
+  """
+
+  @doc """
+  fall_velocity
+    Calculate instantaneous velocity of a falling object
+    that has travelled distance 'distance'.
+
+    Let 'g' represent 'acceleration due to gravity',
+    of which the value is 9.81, then the formula to calculate
+    the instantaneous velocity of a falling object that has
+    travelled distance 'd' will be: 
+    
+      v = :math.sqrt(2gd)
+
+    Params:
+      distance(float):
+        Distance that the falling object has travelled.
+      gravity(float):
+        Gravity coefficient to specify. Defaults to 9.81 on the earth.
+    
+    Returns(float):
+      Instantaneous velocity of the falling object at distance 'distance'.
+  """
+  def fall_velocity(distance, gravity \\ 9.81) do
+    :math.sqrt(2 * gravity * distance)
+  end
+end
